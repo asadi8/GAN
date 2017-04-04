207047a196dda6c12a6392362326f4df67b8f2ec
Chris Grimm <cgrimm@gmail.com>
2017-04-04 18:55:32 -0400

    moop

--- chris_experiment/network.py
+++ chris_experiment/network.py
@@ -40,7 +40,7 @@ with tf.variable_scope('discriminator', reuse=True):
 generator_loss = -tf.reduce_mean(tf.log(DGZ))
 discriminator_loss = -(tf.reduce_mean(tf.log(DX)) + tf.reduce_mean(tf.log(1 - DGZ)))
 
-learning_rate = 0.00001
+learning_rate = 0.000001
 train_gen = tf.train.AdamOptimizer(10*learning_rate).minimize(generator_loss, var_list=nh.get_vars('generator'))
 train_discr = tf.train.AdamOptimizer(learning_rate/50.).minimize(discriminator_loss, var_list=nh.get_vars('discriminator'))
 
