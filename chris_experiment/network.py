38bf227dfd6fcf50f7a21776adf5532711becc96
Chris Grimm <cgrimm@gmail.com>
2017-04-04 19:51:49 -0400

    moop

--- chris_experiment/network.py
+++ chris_experiment/network.py
@@ -10,7 +10,7 @@ def hook_discriminator(inp):
     #    c2 = tf.reshape(c2, [-1, 7*7*10])
 
     with tf.variable_scope('fc1'):
-        fc1 = nh.fullyConnected(tf.reshape(inp, [-1, 28*28]), tf.nn.tanh, 100, bias=0)
+        fc1 = nh.fullyConnected(tf.reshape(inp, [-1, 28*28]), 100, rectifier=tf.nn.tanh, bias=0)
     with tf.variable_scope('fc2'):
         out = nh.fullyConnected(fc1, 1, rectifier=tf.nn.sigmoid, bias=0.0)
     return out
