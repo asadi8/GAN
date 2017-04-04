c845a86f68231d7b466303adc9ece6dc2e381352
Chris Grimm <cgrimm@gmail.com>
2017-04-04 18:13:53 -0400

    moop

--- chris_experiment/main.py
+++ chris_experiment/main.py
@@ -8,10 +8,14 @@ def train_model(num_steps=-1, gen_name='generator', discr_name='discriminator', 
         num_steps = np.inf
     i = 1
     while i < num_steps:
+        gen_loss = '-'
+        discr_loss = '-'
         data = bh.get_batch(32)
         noise = bh.get_noise(32, 10)
-        [_, discr_loss] = network.sess.run([network.train_discr, network.discriminator_loss], feed_dict={network.inp_data: data,
-                                                           network.inp_noise: noise})
+        if i % 5 == 0:
+            [_, discr_loss] = network.sess.run([network.train_discr, network.discriminator_loss], feed_dict={network.inp_data: data,
+                                                               network.inp_noise: noise})
+
         [_, gen_loss, gen_image] = network.sess.run([network.train_gen, network.generator_loss, network.GZ], feed_dict={
             network.inp_data: data,
             network.inp_noise: noise
