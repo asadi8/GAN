8db613ee6f6831a47e5767737316b95408500c11
Chris Grimm <cgrimm@gmail.com>
2017-04-05 02:37:57 -0400

    moop

--- chris_experiment/batch.py
+++ chris_experiment/batch.py
@@ -13,6 +13,7 @@ def get_batch(batch_size):
     batch = []
     for i in range(batch_size):
         indices = np.random.randint(0, len(images), size=5)
+        print images.shape
         sub_batch = np.transpose(images[indices, :, :, [0]], [1, 2, 3, 0])
         batch.append(sub_batch)
     return np.array(batch) / 255.
