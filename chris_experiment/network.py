236026efb9f850048ebb21e8bfb25a6d498b6d84
Chris Grimm <cgrimm@gmail.com>
2017-04-04 21:06:44 -0400

    moop

--- chris_experiment/network.py
+++ chris_experiment/network.py
@@ -19,14 +19,16 @@ def hook_generator(noise):
     with tf.variable_scope('fc1'):
         fc1 = nh.fullyConnected(noise, 100, bias=0.0)
     with tf.variable_scope('fc2'):
-        fc2 = nh.fullyConnected(fc1, 7*7*64, bias=0.0)
-        fc2 = tf.reshape(fc2, [-1, 7, 7, 64])
+        fc2 = nh.fullyConnected(fc1, 1000, bias=0.0)
+    #    fc2 = tf.reshape(fc2, [-1, 7, 7, 64])
+    with tf.variable_scope('fc3'):
+        c2 = tf.reshape(nh.fullyConnected(fc2, 28*28*3, rectifier=tf.nn.sigmoid, bias=0.0), [-1, 28, 28, 3])
     #c2 = nh.fullyConnected(fc2, 28*28, rectifier=tf.nn.sigmoid, bias=0)
     #c2 = tf.reshape(c2, [-1, 28, 28, 1])
-    with tf.variable_scope('c1'):
-        c1 = nh.upConvolution(fc2, 5, 64, 32, bias=0.0)
-    with tf.variable_scope('c2'):
-        c2 = nh.upConvolution(c1, 5, 32, 3, rectifier=tf.nn.sigmoid, bias=0.0)
+    #with tf.variable_scope('c1'):
+    #    c1 = nh.upConvolution(fc2, 5, 64, 32, bias=0.0)
+    #with tf.variable_scope('c2'):
+    #    c2 = nh.upConvolution(c1, 5, 32, 3, rectifier=tf.nn.sigmoid, bias=0.0)
     return c2
 
 inp_data = tf.placeholder(tf.float32, [None, 28, 28, 3])
@@ -43,7 +45,7 @@ with tf.variable_scope('discriminator', reuse=True):
 generator_loss = -tf.reduce_mean(tf.log(DGZ))
 discriminator_loss = -(tf.reduce_mean(tf.log(DX)) + tf.reduce_mean(tf.log(1 - DGZ)))
 
-learning_rate = 0.00001
+learning_rate = 0.0001
 train_gen = tf.train.AdamOptimizer(learning_rate/10.).minimize(generator_loss, var_list=nh.get_vars('generator'))
 train_discr = tf.train.AdamOptimizer(learning_rate).minimize(discriminator_loss, var_list=nh.get_vars('discriminator'))
 
