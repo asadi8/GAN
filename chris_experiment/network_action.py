f38b88c477839987b41a4844d2171ef81dc23006
Chris Grimm <cgrimm@gmail.com>
2017-04-05 17:00:20 -0400

    moop

--- chris_experiment/network_action.py
+++ chris_experiment/network_action.py
@@ -88,8 +88,8 @@ discriminator_loss = -(tf.reduce_mean(tf.log(DX)) + tf.reduce_mean(tf.log(1 - DG
 generator_loss = -tf.reduce_mean(tf.log(DGZ))# + tf.reduce_sum(tf.square(inp_data - GZ))
 
 
-learning_rate = 0.00005
-train_gen = tf.train.AdamOptimizer(5*learning_rate).minimize(generator_loss, var_list=nh.get_vars('generator'))
+learning_rate = 0.00001
+train_gen = tf.train.AdamOptimizer(learning_rate).minimize(generator_loss, var_list=nh.get_vars('generator'))
 train_discr = tf.train.AdamOptimizer(learning_rate).minimize(discriminator_loss, var_list=nh.get_vars('discriminator'))
 
 saver_gen = tf.train.Saver(var_list=nh.get_vars('generator'))
