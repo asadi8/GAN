5950e7b436dcbc46cdf862bfa92adf11e21b9614
Chris Grimm <cgrimm@gmail.com>
2017-04-04 20:28:14 -0400

    moop

--- chris_experiment/batch.py
+++ chris_experiment/batch.py
@@ -5,7 +5,7 @@ import os
 
 dataset_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), '../gray')
 
-images = [cv2.imread(os.path.join(dataset_path, x))[:, :, [0]] for x in os.listdir(dataset_path)
+images = [cv2.imread(os.path.join(dataset_path, x)) for x in os.listdir(dataset_path)
           if x.endswith('.png')]
 images = np.array(images)
 
